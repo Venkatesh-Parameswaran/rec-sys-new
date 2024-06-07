33bf1f0a8d122dc757f2d5785f40b63be689688e
Venkatesh Parameswaran <131514161+Venkatesh-Parameswaran@users.noreply.github.com>
2024-06-07 18:50:03 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -49,7 +49,7 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 
 
 # Example hero image URL (hosted online for reliability)
-hero_img_url = "https://example.com/path_to_your_hero_image.jpg"
+hero_img_url = "https://github.com/Venkatesh-Parameswaran/rec-sys-new/blob/main/hero%20img1.jpg"
 
 # Display hero image
 st.image(hero_img_url, use_column_width=True)
