e0f5818a1694c7ce5343cabbe6186d2e602469ce
Venkatesh Parameswaran <131514161+Venkatesh-Parameswaran@users.noreply.github.com>
2024-06-10 00:13:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ page_bg_img = f'''
     .stApp {{
         background-image: url("{background_image_url}");
         background-size: cover;
-        background-position: center;
+        background-position: top;
         background-repeat: no-repeat;
         background-attachment: fixed;
     }}
