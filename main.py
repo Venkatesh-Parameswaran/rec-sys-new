0a05c0265f06cdabe125f62a246cd43655d250a3
Venkatesh Parameswaran <131514161+Venkatesh-Parameswaran@users.noreply.github.com>
2024-06-09 19:41:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ st.markdown(
         background-attachment: fixed;
     }}
     .stApp {{
-        background-color: rgba(255, 255, 255, 0.7); /* Semi-transparent background for Streamlit elements */
+        background-color: rgba(255, 255, 255, 0); /* Semi-transparent background for Streamlit elements */
         color: #333333; /* Darker gray font color */
         font-size: 18px; /* Font size */
         font-family: Arial, sans-serif; /* Font family */
